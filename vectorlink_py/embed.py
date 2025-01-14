549abc648c6fc7c41031eb12f324a457d2fe7dfd
Gavin Mendel-Gleason <gavin@vectorlink.ai>
2025-01-14 12:31:09 +0100

    Allowing configuration for other vector dimensions

--- vectorlink_py/embed.py
+++ vectorlink_py/embed.py
@@ -1,19 +1,32 @@
 import datafusion as df
 import pyarrow as pa
 from datafusion import DataFrame, SessionContext
+from typing import Dict, Optional
 
 
-embed_table_schema = pa.schema(
-    [
-        pa.field("hash", pa.string_view(), nullable=False),
-        pa.field("embedding", pa.list_(pa.float32(), 1536), nullable=False),
-    ]
-)
+def embedding_schema(configuration: Optional[Dict]):
+    if configuration is not None:
+        dimension = configuration["dimension"]
+    else:
+        dimension = 1536
+
+    embed_table_schema = pa.schema(
+        [
+            pa.field("hash", pa.string_view(), nullable=False),
+            pa.field("embedding", pa.list_(pa.float32(), dimension), nullable=False),
+        ]
+    )
+    return embed_table_schema
 
 
-def get_unembedded(ctx: SessionContext, source: str, destination: str) -> DataFrame:
+def get_unembedded(
+    ctx: SessionContext,
+    source: str,
+    destination: str,
+    configuration: Optional[Dict] = None,
+) -> DataFrame:
     already_vectorized = ctx.read_parquet(
-        destination, schema=embed_table_schema
+        destination, schema=embedding_schema(configuration)
     ).select(df.col("hash"))
     return ctx.read_parquet(source).join(
         already_vectorized, left_on="hash", right_on="hash", how="anti"
