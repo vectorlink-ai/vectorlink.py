1325f1ba696c2809e37609bbb57308da1141206e
Gavin Mendel-Gleason <gavin@vectorlink.ai>
2025-01-14 13:34:11 +0100

    Move vectorize to embed

--- vectorlink_py/embed.py
+++ vectorlink_py/embed.py
@@ -2,6 +2,7 @@ import datafusion as df
 import pyarrow as pa
 from datafusion import DataFrame, SessionContext
 from typing import Dict, Optional
+from . import openai_vectorize
 
 
 def embedding_schema(configuration: Optional[Dict]):
@@ -31,3 +32,24 @@ def get_unembedded(
     return ctx.read_parquet(source).join(
         already_vectorized, left_on="hash", right_on="hash", how="anti"
     )
+
+
+def vectorize(
+    ctx: df.SessionContext,
+    df: df.DataFrame,
+    destination: str,
+    configuration: Optional[Dict] = None,
+) -> df.DataFrame:
+    if configuration is None:
+        configuration = {
+            "provider": "OpenAI",
+            "max_batch_size": 200 * 2 ** 20,
+            "dimension": 1536,
+            "model": "text-embedding-3-small",
+        }
+o
+    if configuration["provider"] == "OpenAI":
+        stream = df.execute_stream()
+        return openai_vectorize.vectorize(ctx, stream, destination, configuration)
+    else:
+        raise Exception("No known vectorization provider")
